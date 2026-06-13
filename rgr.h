068f87b5867e0404b71452140bcdeceecd54a23d
krissmars <k9993005010@gmail.com>
2026-06-13 18:55:14 +0700

    Update rgr.h

--- rgr.h
+++ rgr.h
@@ -11,16 +11,31 @@
 #define ALGORITHM_SCYTALE    5
 #define ALGORITHM_GRONSFELD  6
 
+// вспомогательные функции
 int read_data(const char* path, uint8_t** data, size_t* len);
 int write_data(const char* path, const uint8_t* data, size_t len);
+void secure_zero(void* ptr, size_t len);
 char* generate_key(int algorithm, size_t* key_len);
 
-int xor_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output);
-int playfair_encrypt(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output);
-int playfair_decrypt(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output);
-int caesar_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output, int mode);
-int vigenere_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output, int mode);
-int scytale_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output, int mode);
-int gronsfeld_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, size_t key_len, uint8_t* output, int mode);
+// алгоритмы шифрования
+int xor_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, 
+               size_t key_len, uint8_t* output);
+
+int playfair_encrypt(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                     size_t key_len, uint8_t** output, size_t* output_len);
+int playfair_decrypt(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                     size_t key_len, uint8_t** output, size_t* output_len);
+
+int caesar_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                  size_t key_len, uint8_t* output, int mode);
+
+int vigenere_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                    size_t key_len, uint8_t* output, int mode);
+
+int scytale_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                   size_t key_len, uint8_t** output, size_t* output_len, int mode);
+
+int gronsfeld_cipher(const uint8_t* input, size_t input_len, const uint8_t* key, 
+                     size_t key_len, uint8_t* output, int mode);
 
 #endif
